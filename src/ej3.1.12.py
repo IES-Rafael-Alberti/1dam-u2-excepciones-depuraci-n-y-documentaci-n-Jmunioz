1e1b53348c379c068ed08815e6868714520ca043
Jmunioz <jmunioz@iesrafaelalberti.es>
2023-11-21 21:37:46 +0100

    ok

---
+++ src/ej3.1.12.py
@@ -0,0 +1,4 @@
+"Escribir un programa que almacene las matrices A=(123456) y B=(−100111) en una lista y muestre por pantalla su producto. Nota: Para representar matrices mediante listas usar listas ayhy
+
+def producto(matriz,matriz):
+    
---
+++ src/ej3_04.py
@@ -0,0 +1,8 @@
+loteria=(list in range(1,49))
+loteria=list in range(1,49)(input("Dime los números ganadores: "))
+print(loteria)
+
+while print(loteria):
+    for i  in loteria:
+        loteria.sort()
+        [range(1,49)]
